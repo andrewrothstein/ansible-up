663163298cadac2acde964d352deb1b5e8c0d4f4
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-12-04 04:08:20 +0000

    up:0.21.0

---
+++ README.md
@@ -0,0 +1,39 @@
+andrewrothstein.up
+===========================
+![Build Status](https://github.com/andrewrothstein/up/actions/workflows/build.yml/badge.svg)
+
+Installs Upbounds [up](https://github.com/upbound/up) cli.
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.up
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksum.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://cli.upbound.io
+
+dl()
+{
+    local channel=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
+    local dotexe=${5:-""}
+    local platform="${os}_${arch}"
+    local file=pulumi-v$ver-$platform.$archive_type
+    local url="${MIRROR}/${channel}/${ver}/bin/${platform}/up${dotexe}"
+    local lfile="${DIR}/up_${channel}_${platform}_${ver}${dotexe}"
+
+    if [ ! -e $lfile ];
+    then
+        curl -sSLf -o $lfile $url
+    fi
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dlver () {
+    local channel=$1
+    local ver=$2
+    printf "  %s:\n" $ver
+    dl $channel $ver linux amd64
+    dl $channel $ver linux arm64
+    dl $channel $ver darwin arm64
+    dl $channel $ver darwin amd64
+    dl $channel $ver windows amd64 .exe
+}
+
+dlver stable ${1:-v0.21.0}
